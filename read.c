2d60cc7ed96e8525b5f939218c1e59c759f9c4b7
leolanavo <leo0198@gmail.com>
2016-09-19 19:09:55 -0300

    Funções que envolvem matrizes

---
+++ read.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Cria uma matriz com dimensões lin X col*/
+int** createMatrix (int lin, int col) {
+    int** matrix, i;
+    matrix = malloc(lin * sizeof(int*));
+    for (i = 0; i < lin; i++) {
+        matrix[i] = calloc(col, sizeof(int));
+    }
+    return (matrix);
+}
+
+/*Lê uma matriz lin X col da entrada padrão, entrada por entrada*/
+void readMatrix (int** matrix, int lin, int col) {
+    int i, j;
+    for (i = 0; i < lin; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+/*Libera toda a memória usada para alocar a matriz*/
+void destroyMatrix (int** matrix, int lin) {
+    int i;
+    for (i = 0; i < lin; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+/*Imprime uma matriz linXcol*/
+void printMatrix (int** matrix, int lin, int col) {
+    int i, j;
+    
+    for (i = 0; i < lin; i++) {
+        for (j = 0; j < col; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+    printf("\n");
+    }
+    
+    destroyMatrix(matrix, lin);
+}
+
+int pegSolitaire () {}
+
+int main () {
+    int lin, col, **tab;
+
+    scanf("%d %d", &lin, &col);
+    tab = createMatrix(lin, col);
+    readMatrix(matrix, lin, col);
+    
+    printMatrix(tab, lin, col);
+    return 0;
+}
